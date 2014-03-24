d81cf2706565b0503e5b5f4fa787afdd71e858d8
Peter Olsen <pto@me.com>
2014-03-23 20:48:32 -0400

    floating-point

--- ch02/big.c
+++ ch02/big.c
@@ -3,5 +3,8 @@
 int main()
 {
 	printf("sizeof(int) is %zu\n", sizeof(int));
-	printf("%d\n", 200 * 300 * 400 * 500);
+	printf("%d\n\n", 200 * 300 * 400 * 500);
+
+	printf("sizeof(double) is %zu\n", sizeof(double));
+	printf("%g %g\n", (3.14 + 1e20) - 1e20, 3.14 + (1e20 - 1e20));
 }
