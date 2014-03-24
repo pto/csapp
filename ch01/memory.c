abe1e0f62e34791784de9a14984de1004ee46dba
Peter Olsen <pto@me.com>
2014-03-23 20:42:48 -0400

    just starting

---
+++ ch01/memory.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int a = 42;
+
+int main()
+{
+	int i = 123;
+	printf("address of a is %p, address of i is %p\n", &a, &i);
+}
---
+++ ch02/big.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("sizeof(int) is %zu\n", sizeof(int));
+	printf("%d\n", 200 * 300 * 400 * 500);
+}
