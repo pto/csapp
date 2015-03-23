b3441c0d36e530e7a9dd6ede6d18275b4b8649b7
Peter Olsen <pto@me.com>
2015-03-22 20:43:48 -0400

    starting assembly

--- ch02/infinite.c
+++ ch02/infinite.c
@@ -7,4 +7,6 @@ int main() {
 	printf("-1.0/0.0 is %f\n", -1.0/0.0);
 	printf("1/-1e400 is %f\n", 1/1e400);
 	printf("1.0/(-1.0/0.0) is %f\n", 1.0/(-1.0/0.0));
+	printf("(int)1e10 is %d\n", (int)1e10);
+	printf("(int)(-1.0/0.0) is %d\n", (int)(-1.0/0.0));
 }
